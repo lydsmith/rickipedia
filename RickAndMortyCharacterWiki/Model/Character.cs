f2de9bebf7c0d0a760538a3d38253a0d96fdb21f
lydia <lyd.smith92@gmail.com>
2020-09-19 21:08:03 +0200

    Create Character model

---
+++ RickAndMortyCharacterWiki/Model/Character.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RickAndMortyCharacterWiki.Model
+{
+    public class Character
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string status { get; set; }
+        public string species { get; set; }
+        public string type { get; set; }
+        public string gender { get; set; }
+        public Location origin { get; set; }
+        public Location location { get; set; }
+        public string image { get; set; }
+        public List<string> episode { get; set; }
+        public string url { get; set; }
+        public DateTime created { get; set; }
+    }
+
+    public class Location
+    {
+        public string name { get; set; }
+        public string url { get; set; }
+    }
+}
